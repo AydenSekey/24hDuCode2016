1c9b6af17896c188ef8458d1206e2ad3c81fedd0
Adrien <adrien.duroy@soprasteria.com>
2016-01-07 14:55:25 +0100

    Modification du code de main par du code java 8.

--- src/main/java/fr/soprasteria/App.java
+++ src/main/java/fr/soprasteria/App.java
@@ -1,5 +1,9 @@
 package fr.soprasteria;
 
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello java 8 world !");
+		
+		List<String> strs = new LinkedList<String>();
+		strs.add("str1");
+		strs.add("str2");
+		strs.forEach(t -> System.out.println("lambda : " + t));
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
     }
 }
