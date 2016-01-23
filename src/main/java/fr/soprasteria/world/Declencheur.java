752a8d79c37a92024418af4c12a1ccabbe2614db
Adrien <adrien.duroy@soprasteria.com>
2016-01-23 17:43:34 +0100

    Création d'une interface pour les déclencheurs.

---
+++ src/main/java/fr/soprasteria/world/Declencheur.java
@@ -0,0 +1,11 @@
+package fr.soprasteria.world;
+
+/**
+ * Interface pour les déclencheurs.
+ */
+public interface Declencheur {
+	/**
+	 * Déclenche le déclencheur.
+	 */
+	public void declencher();
+}
