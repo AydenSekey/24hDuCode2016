6cd63d11235b3608dcbb211ef31d25a66554e111
Adrien <adrien.duroy@soprasteria.com>
2016-01-23 22:54:01 +0100

    Correctif de la fabrique + ajout de méthode de fabrication.

--- src/main/java/fr/soprasteria/world/fabriques/FabriqueSimpleWorlds.java
+++ src/main/java/fr/soprasteria/world/fabriques/FabriqueSimpleWorlds.java
@@ -31,11 +31,51 @@ public final class FabriqueSimpleWorlds {
 			}
 		}
 		
+		return world;
+	}
+	
+	/**
+	 * Crée un monde entièrement vide.
+	 * @param nbCols le nombre de colonnes de la grille du monde
+	 * @param nbLis le nombre de lignes de la grille du monde
+	 * @return le monde correspondant.
+	 */
+	public static WorldGrille emptyWorld(int nbCols, int nbLis) {
+		WorldGrille world = new WorldGrille(nbCols, nbLis);
+		CaseVide caseVide = new CaseVide();
+		for(int col = 0; col < nbCols; col++) {
+			for(int li = 0; li < nbLis; li++) {
+					world.setCase(col, li, caseVide);
+			}
+		}
+		return world;
+	}
+	
+	/**
+	 * Crée un monde contenant une cible avec 4 obstacles.
+	 * @return le monde correspondant.
+	 */
+	public static WorldGrille withObstacleWorldExemple1() {
+		int nbCols = 10;
+		int nbLis = 5;
+		int colCible = 1;
+		int liCible = 2;
+		WorldGrille world = new WorldGrille(nbCols, nbLis);
+		CaseVide caseVide = new CaseVide();
+		for(int col = 0; col < nbCols; col++) {
+			for(int li = 0; li < nbLis; li++) {
+				if(li == liCible && col == colCible) {
+					world.setCase(col, li, new Cible());
+				} else {
+					world.setCase(col, li, caseVide);
+				}
+			}
+		}
+		
 		world.setCase(2, 0, new Obstacle());
 		world.setCase(2, 1, new Obstacle());
 		world.setCase(2, 2, new Obstacle());
-		world.setCase(3, 2, new Obstacle());
-		
+		world.setCase(3, 2, new Obstacle());	
 		
 		return world;
 	}
